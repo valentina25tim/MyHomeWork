4b851370830a20e760c284750a2c4f78a46a91d2
Valentina Tim <tina.tinko01@gmail.com>
2022-05-20 10:30:49 +0300

    Added WineHomeWork <- IEnumerable

---
+++ MyHomeWork2.0/MyHomeWork2.0/WineHomeWork/Type.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHomeWork2._0.WineHomeWork
+{
+    internal class Type
+    {
+    }
+}
